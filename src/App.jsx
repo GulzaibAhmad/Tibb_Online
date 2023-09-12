0830b9c47475bd8f4920fc32c427ceabcee4e22b
Gulzaib Ahmad <128983258+GulzaibAhmad@users.noreply.github.com>
2023-09-12 12:26:11 +0500

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Tibb Online</h1>
+      <h2>coming Soon <span>🔃</span></h2>
+    </div>
+  )
+}
+
+export default App
