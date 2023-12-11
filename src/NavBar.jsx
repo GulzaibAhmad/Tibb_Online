af6139acaf7949bc714d88ae5c5109e8526ccdf9
Gulzaib Ahmad <128983258+GulzaibAhmad@users.noreply.github.com>
2023-12-11 16:49:21 +0500

    Update NavBar.jsx

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { AiOutlineClose } from 'react-icons/ai';  
